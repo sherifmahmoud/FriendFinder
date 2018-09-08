585dcee5783b34fe395a4314f542a65634fefc22
Sherif Mahmoud <sherif.h.mahmoud@gmail.com>
2018-09-07 23:27:16 -0500

    setup routing skeleton

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,9 @@
+module.exports = function (app) {
+    app.get("/api/friends", function (req, res) {
+        return res.json();
+    });
+
+    app.post("/api/friends", function (req, res) {
+        return res.json();
+    });
+}
--- server.js
+++ server.js
@@ -3,3 +3,19 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
+
+// Sets up the Express App
+// =============================================================
+const app = express();
+const PORT = 3000;
+require('./routing/htmlRoutes')(app);
+require('./routing/apiRoutes')(app);
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
