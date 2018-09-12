14d3aa84ec49fc2c987a94f02a68cd71d67e8930
Sherif Mahmoud <sherif.h.mahmoud@gmail.com>
2018-09-11 20:32:08 -0500

    Final Submission

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 // Sets up the Express App
 // =============================================================
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
